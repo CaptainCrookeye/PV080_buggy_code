9986118d54a4318c92f7982b53800ea4c53a45bc
CaptainCrookeye <dominiknetri@gmail.com>
2026-05-04 14:47:46 +0200

    fixed some issues in buggy_python

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,7 +1,8 @@
 import sys 
 import os
-import yaml
 import flask
+import yaml
+import importlib
 
 app = flask.Flask(__name__)
 
@@ -14,7 +15,7 @@ def index():
 
         
 CONFIG = {"API_KEY": "771df488714111d39138eb60df756e6b"}
-class Person(object):
+class Person():
     def __init__(self, name):
         self.name = name
 
@@ -25,7 +26,8 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
-    exec(f"import urllib{urllib_version} as urllib", globals())
+    # exec(f"import urllib{urllib_version} as urllib", globals())
+    urllib = importlib.import_module(f"urllib{urllib_version}")
     # Fetch and print the requested URL
  
     try: 
